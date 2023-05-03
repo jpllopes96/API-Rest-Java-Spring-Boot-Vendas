e744a2af9e94b8c169ad465bed48649e00efa4de
jpllopes96 <joaolacaia96@gmail.com>
2023-05-03 20:54:03 -0300

    Iniciando o projeto

---
+++ src/main/java/com/gvendas/gestaovendas/entities/Categoria.java
@@ -0,0 +1,46 @@
+package com.gvendas.gestaovendas.entities;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "categoria")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "codigo")
+    private Long codigo;
+
+    @Column(name = "nome")
+    private String nome;
+
+    public Long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Long codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Categoria categoria)) return false;
+        return Objects.equals(getCodigo(), categoria.getCodigo()) && Objects.equals(getNome(), categoria.getNome());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getCodigo(), getNome());
+    }
+}
